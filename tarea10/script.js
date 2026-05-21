82e9110d934499dd133931c4ffc91b66784ff702
thebrayan006 <thebrayan190301@gmail.com>
2026-05-20 20:23:22 -0400

    Update script.js

--- tarea10/script.js
+++ tarea10/script.js
@@ -1,1 +1,62 @@
+// ENVIAR PREGUNTA
 
+function enviarPregunta(){
+
+let input = document.getElementById("pregunta");
+
+let texto = input.value.trim();
+
+if(texto === "") return;
+
+
+agregarMensaje(texto,"user");
+
+
+// RESPUESTA TEMPORAL
+setTimeout(()=>{
+
+agregarMensaje(
+"Estoy procesando tu pregunta sobre el Estatuto Orgánico de la UASD...",
+"bot"
+);
+
+},800);
+
+
+input.value="";
+
+}
+
+
+// AGREGAR MENSAJES
+
+function agregarMensaje(texto,tipo){
+
+let chatBox = document.getElementById("chatBox");
+
+let mensaje = document.createElement("div");
+
+mensaje.classList.add("mensaje");
+mensaje.classList.add(tipo);
+
+mensaje.innerText = texto;
+
+chatBox.appendChild(mensaje);
+
+
+// SCROLL AUTOMATICO
+
+chatBox.scrollTop = chatBox.scrollHeight;
+
+}
+
+
+// PREGUNTAS SUGERIDAS
+
+function preguntaSugerida(boton){
+
+document.getElementById("pregunta").value = boton.innerText;
+
+enviarPregunta();
+
+}
